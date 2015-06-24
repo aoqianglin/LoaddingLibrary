18e173c998c23010ac8e4deae5d1c07f03093728
aoqianglin <aoqianglin@xiguo.cc>
2015-06-24 21:42:08 +0800

    优化圆形图片加载进度条

--- src/com/dafruits/android/library/widgets/CircleLoaddingView.java
+++ src/com/dafruits/android/library/widgets/CircleLoaddingView.java
@@ -1,153 +1,221 @@
 package com.dafruits.android.library.widgets;
 
-import android.animation.ValueAnimator;
-import android.animation.ValueAnimator.AnimatorUpdateListener;
+import java.lang.ref.WeakReference;
+
 import android.annotation.SuppressLint;
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
-import android.graphics.Path;
 import android.graphics.Paint.Style;
+import android.graphics.Path;
 import android.graphics.RectF;
+import android.os.Handler;
+import android.os.Message;
 import android.util.AttributeSet;
-import android.util.Log;
-import android.view.View;
-import android.view.animation.LinearInterpolator;
+import android.util.DisplayMetrics;
+import android.widget.ImageView;
 
 /**
- * 通过绘制方式实现，圆形加载
  * 
- * @author Ryze
- * @see AppLoaddingImage 利用两张图片实现
+ * @description 圆形进度加载
+ * @autor Ryze 2015-5-27 下午3:05:05
+ * 
  */
-@SuppressLint("NewApi")
-public class CircleLoaddingView extends View implements AnimatorUpdateListener{
+public class CircleLoaddingView extends ImageView {
 
-	private int width = 0;
 
-	private int height = 0;
-	
-	private int radius=0;
+  private int width;
 
-	private Paint bg_paint;
-	private RectF bg_RectF;
-	
+  private int height;
 
-	private Paint moving_paint;
-	private Path moving_Path;
-	private ValueAnimator moving_Animator;
-	
-	private int degree=0;
+  private int bg_color = Color.parseColor("#64000000");
 
-	public CircleLoaddingView(Context context, AttributeSet attrs,
-			int defStyleAttr, int defStyleRes) {
-		super(context, attrs, defStyleAttr, defStyleRes);
-		init();
-	}
+  private Paint bg_Paint;
 
-	public CircleLoaddingView(Context context, AttributeSet attrs,
-			int defStyleAttr) {
-		super(context, attrs, defStyleAttr);
-		init();
-	}
+  private float radius;
 
-	public CircleLoaddingView(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		init();
-	}
 
-	public CircleLoaddingView(Context context) {
-		super(context);
-		init();
-	}
+  private int draw_color = Color.parseColor("#ffffff");
 
-	private void init() {
+  private Paint draw_Paint;
 
-		bg_paint=new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-		bg_paint.setColor(Color.parseColor("#555566"));
-		bg_paint.setStyle(Style.STROKE);
-		bg_paint.setStrokeWidth(15);
-		bg_RectF=new RectF();
-		
-		moving_paint= new Paint(Paint.ANTI_ALIAS_FLAG|Paint.FILTER_BITMAP_FLAG);
-		moving_paint.setColor(Color.parseColor("#ff5566"));
-		moving_paint.setStyle(Style.STROKE);
-		moving_paint.setStrokeWidth(15);
-		moving_Path=new Path();
-		
-		
-	}
-	
-	private void startAnimation(){
-		if (moving_Animator!=null) {
-			moving_Animator.cancel();
-			moving_Animator.removeAllListeners();
-			moving_Animator.removeAllUpdateListeners();
-			moving_Animator=null;
-		}
-		moving_Animator=ValueAnimator.ofInt(1,360);
-		moving_Animator.setDuration(2000);
-		moving_Animator.setInterpolator(new LinearInterpolator());
-		moving_Animator.setRepeatMode(ValueAnimator.INFINITE);
-		moving_Animator.setRepeatCount(ValueAnimator.INFINITE);
-		moving_Animator.addUpdateListener(this);
-		moving_Animator.start();
-	}
-	
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
-		super.onSizeChanged(w, h, oldw, oldh);
-		width=getWidth();
-		height=getHeight();
-		
-		if (width==0 || height==0) {
-			return ;
-		}
-		bg_RectF.setEmpty();
-		moving_Path.reset();
-		
-		if (width>height) {
-			radius=height/4;
-		}else {
-			radius=height/4;
-		}
-		
-		bg_RectF.set(width/2-radius, height/2-radius, width/2+radius, height/2+radius);
-		moving_Path.addArc(bg_RectF, 0, 45);
-		
-		startAnimation();
-	}
-	
-	@Override
-	protected void onDraw(Canvas canvas) {
-		super.onDraw(canvas);
-		
-		canvas.drawCircle(width/2, height/2, radius, bg_paint);
-		canvas.rotate(degree,width/2,height/2);
-		canvas.drawPath(moving_Path, moving_paint);
-//		canvas.restore();
-	}
+  private RectF draw_RectF;
+  private Path path;
 
-	@Override
-	public void onAnimationUpdate(ValueAnimator animation) {
-     degree=(Integer) animation.getAnimatedValue();
-     Log.e(VIEW_LOG_TAG, "degree: "+degree);
-     invalidate();
-	}
-	
-	@Override
-	protected void onVisibilityChanged(View changedView, int visibility) {
-		super.onVisibilityChanged(changedView, visibility);
-		if (visibility==VISIBLE) {
-			if (moving_Animator!=null) {
-				moving_Animator.start();;
-			}
-		}else {
-			if (moving_Animator!=null) {
-				moving_Animator.cancel();;
-			}
-		}
-	}
+
+  private int bg_cirlce_color = Color.parseColor("#05000000");
+  private Paint bg_circle_Paint;
+
+
+  private float progressValue = 0;
+
+  private float current_value = 0;
+
+  private float degree = 0;
+
+
+  private ProgressHandler handler;
+
+  /**
+   * @param context
+   * @param attrs
+   * @param defStyleAttr
+   * @param defStyleRes
+   */
+  @SuppressLint("NewApi")
+  public CircleLoaddingView(Context context, AttributeSet attrs, int defStyleAttr,
+      int defStyleRes) {
+    super(context, attrs, defStyleAttr, defStyleRes);
+    init();
+  }
+
+  /**
+   * @param context
+   * @param attrs
+   * @param defStyleAttr
+   */
+  public CircleLoaddingView(Context context, AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  /**
+   * @param context
+   * @param attrs
+   */
+  public CircleLoaddingView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  /**
+   * @param context
+   */
+  public CircleLoaddingView(Context context) {
+    super(context);
+    init();
+  }
+
+
+  private static class ProgressHandler extends Handler {
+
+    private CircleLoaddingView loaddingImageView;
+
+    /**
+     * @param loaddingImageView
+     */
+    public ProgressHandler(CircleLoaddingView loaddingImageView) {
+      super();
+      this.loaddingImageView = new WeakReference<CircleLoaddingView>(loaddingImageView).get();
+    }
+
+    @Override
+    public void handleMessage(Message msg) {
+      super.handleMessage(msg);
+      if (loaddingImageView == null) {
+        return;
+      }
+      loaddingImageView.degree = (Float)msg.obj;
+      loaddingImageView.postInvalidate();
+    }
+  }
+
+  /**
+   * 
+   * 获取屏幕高宽
+   * 
+   * @param 第一个参数为宽 第二个为高
+   * @return
+   */
+  public int[] getScreenSize(Context mContext) {
+    DisplayMetrics dm = new DisplayMetrics();
+    ((Activity) mContext).getWindowManager().getDefaultDisplay().getMetrics(dm);
+    int nowWidth = dm.widthPixels; // 当前分辨率 宽度
+    int nowHeigth = dm.heightPixels; // 当前分辨率高度
+
+    return new int[] {nowWidth, nowHeigth};
+  }
+
+  private void init() {
+    bg_Paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+    bg_Paint.setColor(bg_color);
+    bg_Paint.setStyle(Style.FILL);
+
+    draw_Paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+    draw_Paint.setColor(draw_color);
+    draw_Paint.setAntiAlias(true);
+    draw_Paint.setStrokeWidth(18);
+    draw_Paint.setStyle(Style.STROKE);
+    draw_RectF = new RectF();
+    path = new Path();
+
+    bg_circle_Paint = new Paint(Paint.ANTI_ALIAS_FLAG | Paint.FILTER_BITMAP_FLAG);
+    bg_circle_Paint.setColor(bg_cirlce_color);
+    bg_circle_Paint.setStrokeWidth(18);
+    bg_circle_Paint.setStyle(Style.STROKE);
+
+    handler = new ProgressHandler(this);
+  }
+
+  public void updateProgress(int total, int progress) {
+    if (handler == null || total == 0) {
+      return;
+    }
+    this.progressValue = total;
+    this.current_value = progress;
+    handler.sendMessageDelayed(handler.obtainMessage(1, current_value / progressValue), 100);
+
+  }
+
+  /**
+   * 进度清零,重新设进度
+   */
+  public void reSetProgress() {
+    progressValue = 0f;
+    current_value = 0f;
+    handler.sendMessageDelayed(handler.obtainMessage(1, 0f), 200);
+  }
+
+  @Override
+  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+    super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    setMeasuredDimension(getMeasuredWidth(), getMeasuredWidth());
+  }
+
+  @Override
+  protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+    super.onSizeChanged(w, h, oldw, oldh);
+
+    this.width = getWidth();
+    this.height = getHeight();
+
+    radius = getScreenSize(getContext())[0] / 20.0f;
+
+    draw_RectF
+        .set(width / 2 - radius, height / 2 - radius, width / 2 + radius, height / 2 + radius);
+  }
+
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+    if (degree == 0) {
+      // canvas.restore();
+    } else {
+      canvas.restore();
+
+      canvas.drawCircle(width / 2, height / 2, radius + radius / 2, bg_Paint);
+      canvas.save();
+
+      canvas.drawArc(draw_RectF, 0, 360, true, bg_circle_Paint);
+      canvas.restore();
+      path.addArc(draw_RectF, -90, degree * 360);
+      canvas.drawPath(path, draw_Paint);
+    }
+    canvas.restore();
+
+  }
 
 }
